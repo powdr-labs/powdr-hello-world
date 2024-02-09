d644cf51b71b85eb2e6e426b5e480134da17cab8
Leo Alt <leo@ethereum.org>
2024-02-09 18:17:44 +0100

    add README

--- src/main.rs
+++ src/main.rs
@@ -16,7 +16,7 @@ fn main() {
 
     // Step-by-step case
 
-    // First we create the universal setup of size 8
+    // First we create the universal setup of size 8 needed by Halo2
     let backend = BackendType::Halo2.factory::<Bn254Field>().create(8);
 
     let mut params_file = BufWriter::new(File::create("params.bin").unwrap());
@@ -43,7 +43,7 @@ fn main() {
         .proof
         .unwrap();
 
-    // Create a fresh pipeline for proof verification
+    // Create a fresh pipeline for separate proof verification
     let mut pipeline = pipeline
         .with_backend(BackendType::Halo2)
         .with_setup_file(Some("params.bin".into()))
